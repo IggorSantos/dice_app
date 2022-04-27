4305fc5a8bea6faaf4358cd4bdb23478d662d591
Igor Santos <igor.sagitarius@gmail.com>
2022-04-27 11:34:09 -0300

    Primeiro commit

---
+++ lib/main.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  return runApp(
+    MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.red,
+        appBar: AppBar(
+          title: Text('Dicee'),
+          backgroundColor: Colors.red,
+        ),
+        body: DicePage(),
+      ),
+    ),
+  );
+}
+
+class DicePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+        child: Row(
+        children: <Widget>[
+          Expanded(
+          child: Padding(
+            padding: EdgeInsets.all(16),
+            child: Image.asset('images/dice1.png'),//Uma Forma mais simples de escrever o mesmo codigo abaixo
+            ),
+          ),
+          Expanded(
+            child: Padding(
+              padding: EdgeInsets.all(16),
+              child: Image(
+              image: AssetImage('images/dice1.png')
+              ),
+            ),
+          ),
+        ],
+      )
+   );
+  }
+}
